d8ecb04375c265f5244b7f6edcf98797ecbdd809
Mrigank Pawagi <mrigankpawagi@gmail.com>
2023-08-06 13:33:23 +0530

    biobricks data

---
+++ get_bb.py
@@ -0,0 +1,45 @@
+with open("parts.txt") as f:
+    text = f.read()
+
+parts_raw = [x.strip() for x in text.split(">")][1:]
+parts = []
+desc_lens = {}
+
+for p in parts_raw:
+    id = p.split(maxsplit=1)[0]
+    part_type = p.split("\n", maxsplit=1)[0].split()[3]
+    description = p.split('"', 1)[1].split('"', 1)[0]
+
+    if len(description) not in desc_lens:
+        desc_lens[len(description)] = 0
+    desc_lens[len(description)] += 1
+
+    parts.append({
+        "id": id,
+        "type": part_type,
+        "description": description
+    })
+
+
+# Create a plot on the distribution of description lengths
+
+# import matplotlib.pyplot as plt
+
+# plt.bar(desc_lens.keys(), desc_lens.values())
+# plt.xlabel("Description length")
+# plt.ylabel("Number of parts")
+# plt.title("Distribution of description lengths")
+# plt.show()
+
+
+# Parts with descriptions of length 0 to 1 (16 such parts)
+# These are poor descriptions, and should be modified!
+
+for_improvement = [x for x in parts if len(x["description"]) < 2]
+
+# Parts with descriptions of length over 110 (27 such parts)
+# These are good descriptions, and should be used for training!
+
+for_training = [x for x in parts if len(x["description"]) > 110]
+
+
