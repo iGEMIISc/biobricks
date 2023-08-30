d6f0543d39bef06095cccc0f466caf2e3f99145a
Mrigank Pawagi <mrigankpawagi@gmail.com>
2023-08-30 18:15:42 +0530

    add part pages to CSV

--- build_csv.py
+++ build_csv.py
@@ -9,12 +9,14 @@ parts = []
 
 for part in parts_raw:
     line = part.split("\n")[0].split(maxsplit=4)
+    part_id = line[0]
+    part_link = f"https://parts.igem.org/Part:{part_id}"
     part_type = line[3]
     part_desc = line[4].strip("\"")
-    parts.append((part_type, part_desc, '', '', ''))
+    parts.append((part_link, part_type, part_desc, '', '', ''))
 
 random.shuffle(parts)
 
 with open("parts.csv", "w") as f:
     writer = csv.writer(f)
-    writer.writerows([('Type', 'Description', 'Rating1', 'Rating2', 'Rating3')] + parts)
+    writer.writerows([('Part Webpage', 'Type', 'Description', 'Rating1', 'Rating2', 'Rating3')] + parts)
